ef4c914589a3911bf8f1ee16112341bb4c5a2750
施秉志 <b03902039@ntu.edu.tw>
2015-05-29 09:47:40 +0800

    5/29 debug&rename
    after_transformed->encoded, before_transformed->raw

--- main_functon.h
+++ main_functon.h
@@ -4,24 +4,24 @@
 template <class H>
 class Costumer{
 private:
-	string after_transformed_password;;
+	string encoded_password;
 	int deposit;
 	H history;
 
 public:
 	string ID;
-	Costumer(string idendity,string before_transformed_password){//constructor;
+	Costumer(string idendity,string raw_password){//constructor;
 		ID  = idendity;
-		after_transformed_password = encode(before_transformed_password); //encode is ALO's encoding function;
+		encoded_password = encode(raw_password); //encode is ALO's encoding function;
 		deposit = 0;
 	};
 	~Costumer(){
 		delete ID;
-		delete after_transformed_password;
+		delete encoded_password;
 		delete history;
 	}
-	bool authenticated(string before_transformed_password){//authentication;
-		return (encode(before_transformed_password) == after_transformed_password);
+	bool authenticated(string raw_password){//authentication;
+		return (encode(raw_password) == encoded_password);
 	};
 	void Deposit(int money){
 		deposit += money;
