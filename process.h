fbd3e6b8c2c564d68f6ddde7734f132d15a799ce
Guest <guest-aXySyC@ta217-OptiPlex-380.(none)>
2015-06-09 14:12:34 +0800

    main edited, process added //6.9

---
+++ process.h
@@ -0,0 +1,12 @@
+#include "main_function.h"
+using namespace std;
+
+void processLogin();
+void processCreate();
+void processDelete();
+void processMerge();
+void processDeposit();
+void processWithdraw();
+void processTransfer();
+void processFind();
+void processSearch();
