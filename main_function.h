0138951f06737c30ea925a4a4980cceee83fb33c
b03902001 <b03902001@linux13.csie.ntu.edu.tw>
2015-06-12 22:44:11 +0800

    transfer_v3 merge_v1 "history modify, stl merge"

--- main_function.h
+++ main_function.h
@@ -13,13 +13,20 @@
 #include <set>
 #define TO 0
 #define FROM 1
+using namespace std;
 
 struct transferhistory{
-	bool tofrom;
 	string id;
+	int priority;
+	bool tofrom;
 	int money;
+	struct transferhistory *bro;
 }; 
 typedef struct transferhistory THistory;
+
+bool mergecmp(THistory *a, THistory *b){
+	return(a->priority <= b->priority);
+}
 
 class INDEX{
 	public:
@@ -75,27 +82,44 @@ public:
 		history.add(2,money);//not sure
 		return;
 	}
-	void merge(Customer id2){
+	void mergehistory(Customer id2){
 		deposit += id2.deposit;
-		history.merge(id2.history);
+		vector<THistory *> vtmp;
+		merge(history, id2.history, vtmp, mergecmp);
+		history = vtmp;
+			
+		vector<THistory *>::iterator i;
+		for(i = id2.history.begin(); i != id2.history.end(); i++){
+			(*i)->bro->id = ID;
+			(*i)->bro->bro = NULL;
+			delete (*i);
+		}
+		//history.merge(id2.history);/*I want to use stl merge*/
 		delete id2; 
 		return;
 	}
-	void transfer(Customer *target, int tsf_money){ // require Alo's transfer function
+	void transfer(Customer *target, int tsf_money, int p){ // require Alo's transfer function
 		if(tsf_money > deposit)
 			printf("fail, [%d] dollars only in current account\n", deposit);
 		else{
 			deposit -= tsf_money;
 			target->deposit += tsf_money;
 			
-			THistory tmp;
-			tmp.money = tsf_money;
-			tmp.id = ID;
-			tmp.tofrom = TO;
+			THistory *tmp, *tmp2;
+			tmp = new THistory;
+			tmp2 = new THistory;
+			
+			tmp->money = tmp2->money = tsf_money;
+			tmp->priority = tmp2->priority = p;
+			tmp->id = target->ID;
+			tmp->tofrom = TO;
+			tmp->bro = tmp2;
 			history.push_back(tmp);
-			tmp.id = target->ID;
-			tmp.tofrom = FROM;
-			target->history.push_back(tmp);
+			
+			tmp2->id = ID;
+			tmp2->tofrom = FROM;
+			tmp2->bro = tmp1;
+			target->history.push_back(tmp2);
 			printf("success, [%d] dollars left in current account\n", deposit);
 		}
 		return;
