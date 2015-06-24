325e060f3b5a6129f0e91b6d04141489b42278d8
Guest <guest-Gt9qDK@ta217-OptiPlex-380.(none)>
2015-06-24 18:36:36 +0800

    classification

---
+++ function/list10.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <set>
+#include <ctime>
+#include "score.h"
+using namespace std;
+#define MAX 100
+
+class TL{
+public:
+	string id;
+	int score;
+	bool operator<(const TL &a)const{
+		if(score != a.score)
+			return score < a.score;
+		else
+			return id < a.id;
+	}
+	TL(string str, int n){
+		id = str;
+		score = n;
+	}
+};
+
+class Customer{
+public:
+	int a;
+};
+
+class INDEX{
+	public:
+	string id;
+	Customer *cu_ptr;
+	bool operator < (const INDEX &a)const{return id < a.id;}
+	bool operator > (const INDEX &a)const{return id > a.id;}
+	bool operator == (const INDEX &a)const{return id == a.id;}
+	bool operator == (const string &a)const{return a == id;}
+	/*constructor & destructor*/
+	INDEX(){
+		id = "";
+		cu_ptr = NULL;
+	}
+	INDEX(string st){
+		id = st;
+		cu_ptr = NULL;
+	}
+	INDEX(string st, Customer ptr){
+		id = st;
+		cu_ptr = &ptr;
+	}
+	~INDEX(){
+		id.clear();
+		delete cu_ptr;
+	}
+	// other function
+};
+
+/*int ctable[20] = {1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 
+	66, 78, 91, 105, 120, 136, 153, 171, 190, 210}*/ /* table of valid n_len */
+
+const string strtable = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+void all_digit_change(int h, vector<int> &NC, set<string> &listset2, string &sample, int pos, const string origin){
+	if(h > NC.size()){
+		listset2.insert(sample);
+		return;
+	}
+	for(int i = 0; i < 62; i++){
+		if(strtable[i] == origin[pos-NC[h-1]])
+			continue;
+		sample[pos-NC[h-1]] = strtable[i];
+		all_digit_change(h+1, NC, listset2, sample, pos, origin);
+	}
+}
+
+void pick_or_not(int n, int h, vector<int> NC, set<string> &listset2, string sample, int pos){
+	if(n < h && n != 0){
+			//cout << "fuck!!!" << endl;
+		return;
+	}else if(n == 0){
+			//cout << "yeah!!!" << endl;
+		all_digit_change(1, NC, listset2, sample, pos, sample);
+		return;
+	}
+	pick_or_not(n, h+1, NC, listset2, sample, pos); // not pick
+	NC.push_back(h); 
+	pick_or_not(n-h, h+1, NC, listset2, sample, pos); // pick
+	//cout << "fuck yeah!!!" << endl;
+}
+
+void extend(int cnt, int n_var, set<string> &listset2, string ID, int pos){
+	if(cnt == 0){
+		vector<int> needChange; 
+		pick_or_not(n_var, 1, needChange, listset2, ID, pos);
+		return;
+	}
+	for(int i = 0; i < 62; i++){
+		string ex_string = ID + strtable[i];
+		extend(cnt - 1, n_var, listset2, ex_string, pos);
+	}
+}
+
+void clear_exist_ID(set<string> &listset2, set<INDEX> idset){
+	set<string>::iterator ti;
+	for(ti = listset2.begin(); ti != listset2.end(); ti++){
+		if(idset.find(INDEX(*ti)) != idset.end()){
+			//cout <<"clear "<< *ti << endl;
+			listset2.erase(ti);
+		}
+	}
+}
+
+
+void listing10(bool exist, string ID, set<INDEX> &idset){
+	int i;
+	set<INDEX>::iterator si;
+	set<TL> listset;
+	set<TL>::iterator ti;
+	if(exist){
+		for(si = idset.begin(); si != idset.end(); si++){
+				listset.insert(TL(si->id, score(ID, si->id)));
+		}
+		for(i = 0, ti = listset.begin(); i < 10 && ti != listset.end(); ti++){
+			if(ti->score != 0){
+				cout << ti->id <<", "<< ti->score << endl;
+				i++;
+			}
+		}
+	}else{
+		set<string> listset2;
+		set<string>::iterator tt;
+		vector<int> needChange;
+		int num_in_set = listset2.size(), lv, n_len, n_var, cnt;
+		for(lv = 1; num_in_set < 10; lv++){
+			/* shorten length */
+			for(n_len = 1, cnt = 1; n_len <= lv; cnt++, n_len+=cnt){
+				n_var = lv - n_len;
+				if(ID.size() > cnt){
+					string cut_string(ID, 0, ID.size()-cnt);
+					pick_or_not(n_var, 1, needChange, listset2, cut_string, cut_string.size());
+				}else
+					break;
+			}
+			/* same length */
+				pick_or_not(lv, 1, needChange, listset2, ID, ID.size());
+			/* extend length */
+			for(n_len = 1, cnt = 1; n_len <= lv; cnt++, n_len+=cnt){
+				n_var = lv - n_len;
+				extend(cnt, n_var, listset2, ID, ID.size());
+			}
+			clear_exist_ID(listset2, idset);
+			for(tt = listset2.begin(); tt != listset2.end(); tt++){
+				listset.insert(TL(*tt, lv));
+			}
+			needChange.clear();
+			num_in_set = listset.size();
+			if(num_in_set >= 10){
+				for(i = 0, ti = listset.begin(); i < 10; ti++){
+					if(ti->id != ID){
+						cout << ti->id <<", "<< ti->score << endl;
+						i++;
+					}
+				}
+				break;
+			}
+		}
+	}
+}
+
+int main(int argc, char** argv){
+	time_t t1, t2;
+	set<INDEX> idset;
+	ifstream fp(argv[1]);
+	INDEX *tmp = new INDEX;
+	while(getline(fp, tmp->id))
+		idset.insert(*tmp);
+	delete tmp;
+	fp.close();
+
+	cout << " #Read in finished" << endl;
+	
+	cout << endl << " $Type something to search : ";
+	char cstr[100];
+	while(scanf("%s", cstr) != EOF){
+		bool can_process = true;
+		string *tmp = new string(cstr);
+		if(tmp->size() == 0){
+			cout << " #please type something" << endl;
+			continue;			
+		}
+		if((*tmp) == "@"){
+			cout << " #See you next time! Bye!" << endl;
+			return 0;
+		}
+			t1 = time(NULL);
+		cout << " $The following is the top 10 ID recommended" << endl;
+		listing10(false, *tmp, idset);
+			t2 = time(NULL);
+			printf("The reading time is %0ld:%0ld (min:sec)\n", (t2-t1)/60, (t2-t1)%60);
+		cout << "================================" << endl;
+		cout << "$Type something to search : ";
+	}
+	return 0;
+}
---
+++ function/yfind.cpp
@@ -0,0 +1,259 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <set>
+#include <vector>
+using namespace std;
+#define MAX 100
+
+class Customer{
+public:
+	int a;
+};
+
+class INDEX{
+	public:
+	string id;
+	Customer *cu_ptr;
+	bool operator < (const INDEX &a)const{return id < a.id;}
+	bool operator > (const INDEX &a)const{return id > a.id;}
+	bool operator == (const INDEX &a)const{return id == a.id;}
+	bool operator == (const string &a)const{return a == id;}
+	/*constructor & destructor*/
+	INDEX(){
+		id = "";
+		cu_ptr = NULL;
+	}
+	INDEX(string st, Customer ptr){
+		id = st;
+		cu_ptr = &ptr;};
+	~INDEX(){
+		id.clear();
+		delete cu_ptr;
+	}
+	// other function
+};
+
+enum prcsmode {CHR, QMK, STAR};
+
+void processFind(string user, string test, set<INDEX>& idset){
+	int mode = -1;
+	bool found = false;
+	set<INDEX>::iterator si;
+	if(test.find("*") == string::npos && test.find("?") == string::npos){
+		for(si = idset.begin(); si != idset.end(); si++){
+			if(test == si->id){
+				cout << si->id << endl;
+				found = true;
+			}
+		}
+		if(!found)
+			cout /*<< " #Not Found" */<< endl;
+		return;
+	}
+	vector<string> fragment;
+	string current;
+	size_t bottom; // top, bottom not always exist
+	bool top_exist = true, bottom_exist = true, all_qmk = false;// for non-star
+	int D[MAX] = {0}, bottom_dis = 0; // for qmk
+	// process the search string
+	if(test[0] == '*')
+		top_exist = false; 
+	for(int i = 0; i < test.size(); i++){ 
+		if(test[i] == '*'){
+			if(mode == -1){
+				mode = STAR;
+				continue;
+			}
+			mode = STAR;
+			fragment.push_back(current);
+			current.clear();
+			D[fragment.size()] = 0;
+		}else if(test[i] == '?'){
+			if(mode == CHR){ 
+				fragment.push_back(current);
+				current.clear();
+				D[fragment.size()] = 2;
+				mode = QMK;
+			}else if(mode == -1){ // head = ? condition
+				D[0]++;
+			}else {
+				D[fragment.size()]++;
+				mode = QMK;
+			}
+		}else {
+			current += test[i];
+			mode = CHR;
+		}
+	}
+	if(mode == CHR){ // push the last fragment
+		fragment.push_back(current);
+		current.clear();
+		bottom_dis = 0;
+	}else if(mode == QMK){
+		bottom_dis = D[fragment.size()]-1;
+	}else if(mode == STAR)
+		bottom_exist = false; 
+	if(fragment.size() == 0 && mode == -1){
+		//cout << "all qmk, " << D[0] << endl;
+		all_qmk = true;
+	}
+
+	
+	// go through our idset
+	vector<string>::iterator fi;
+	int count = 0;
+	
+	for(fi = fragment.begin(), count = 0; fi != fragment.end(); fi++, count++){
+		cout << (*fi) << ", " << D[count] << ", " << endl;
+	}
+	if(all_qmk){
+		for(si = idset.begin(); si != idset.end(); si++){
+			if((si->id).size() == D[0]){
+				cout << (si->id) << endl;
+				found = true;
+			}
+		}
+		if(!found)
+			cout /*<< " #Not Found" */<< endl;
+		return;
+	}
+
+	for(si = idset.begin(); si != idset.end(); si++){
+		size_t last = 0;
+		bool can_print = true;
+		bool top_accord = true; // assumed it will be ok
+		bool bottom_accord = true; // assumed it will be ok
+		bool mid_accord = true; // assumed it will be ok
+		bool find = true; // assumed it will be ok
+		for(fi = fragment.begin(), count = 0; fi != fragment.end(); fi++, count++){
+			size_t start = 0, current;
+			while(1){
+				if((current = (si->id).find(*fi, start)) == string::npos){
+					find = false;
+					break;
+				}
+				if(fi == fragment.begin()){ // check top
+					if(top_exist && current < D[0]){
+						start = current+1;
+						continue;
+					}else if(top_exist && current > D[0]){
+						top_accord = false;
+						break;
+					}
+					if(fi == fragment.end() - 1 && bottom_exist){ // check bottom
+						if(current+(*fi).size()+bottom_dis < (si->id).size()){
+							start = current+1;
+							continue;
+						}else if(current+(*fi).size()+bottom_dis > (si->id).size())
+							bottom_accord = false;
+					}
+					break;
+				}
+				if(D[count] <= 0){ // check fragment after *
+					if(current <= last){
+						start = current+1;
+						continue;
+					}
+					if(fi == fragment.end() - 1 && bottom_exist){ // check bottom
+						if(current+(*fi).size()+bottom_dis < (si->id).size()){
+							start = current+1;
+							continue;
+						}else if(current+(*fi).size()+bottom_dis > (si->id).size())
+							bottom_accord = false;
+					}
+					break;
+				}else{ // check fragment after ?
+					if(current-last < D[count]){
+						start = current+1;
+						continue;
+					}else if(current-last > D[count]){
+						mid_accord = false;
+						break;
+					}
+					if(fi == fragment.end() - 1 && bottom_exist){ // check bottom
+						if(current+(*fi).size()+bottom_dis < (si->id).size()){
+							start = current+1;
+							continue;
+						}else if(current+(*fi).size()+bottom_dis > (si->id).size())
+							bottom_accord = false;
+					}
+					break;
+				}
+			}
+			last = current + (*fi).size() - 1;
+			if(!find || !top_accord || !mid_accord || !bottom_accord){
+				can_print = false;
+				break;
+			}
+		}
+		if(can_print && si->id != user){
+			/* print out the according word */
+			cout << (si->id) << endl;
+			found = true;
+		}
+	}
+	if(!found)
+		cout /*<< " #Not Found" */<< endl;
+}
+
+int main(int argc, char** argv){
+	set<INDEX> ID;
+	ifstream fp(argv[1]);
+	INDEX *tmp = new INDEX;
+	while(getline(fp, tmp->id))
+		ID.insert(*tmp);
+	delete tmp;
+	fp.close();
+
+	cout << " #Read in finished" << endl;
+	cout << " $From now on, input words to search" << endl;
+	cout << " // word's length Domain : 1 ~ 100" << endl;
+	cout << " // each charater can only be in {0~9, A~Z, a~z, ?, *}" << endl;
+	cout << " // ? : one charater that can be any of {0~9, A~Z, a~z}" << endl;
+	cout << " // * : any number(in Domain or 0) of charaters that can be any of {0~9, A~Z, a~z}" << endl;
+	cout << " // it is invalid to type such as **, *?, ?* " << endl;
+
+	cout << endl << " $Type something to search : ";
+	char cstr[100];
+	while(scanf("%s", cstr) != EOF){
+		bool can_process = true;
+		string *tmp = new string(cstr);
+		if(tmp->size() == 0){
+			cout << " #please type something" << endl;
+			continue;			
+		}
+		if((*tmp) == "@"){
+			cout << " #See you next time! Bye!" << endl;
+			return 0;
+		}
+		for(int i = 0; i < tmp->size(); i++){
+			if((*tmp)[i] < '0' || ((*tmp)[i] > '9' && (*tmp)[i] < 'A') 
+				|| ((*tmp)[i] > 'Z' && (*tmp)[i] < 'a') || (*tmp)[i] > 'z'){
+				if((*tmp)[i] != '*' && (*tmp)[i] != '?'){
+					cout << " #there's invalid charater : " << (*tmp)[i] << endl;
+					can_process = false;
+				}
+			}
+			if((*tmp)[i] == '*' && i > 0){
+				if((*tmp)[i-1] == '*' || (*tmp)[i-1] == '?'){
+					cout << " #there's invalid combination : " << (*tmp)[i-1] << (*tmp)[i] << endl;
+					can_process = false;
+				}
+			}
+			if((*tmp)[i] == '?' && i > 0 && (*tmp)[i-1] == '*'){
+				cout << " #there's invalid combination : " << (*tmp)[i-1] << (*tmp)[i] << endl;
+				can_process = false;
+			}
+		}
+		if(can_process){
+			string *user_now = new string;
+			cin >> *user_now; 
+			processFind(*user_now, *tmp, ID);
+		}
+		cout << "$Type something to search : ";
+	}
+	return 0;
+}
